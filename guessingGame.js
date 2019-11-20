73d9a4df5026c06d133017fafc5dc79a65e0b3b1
mrbdahlem <DAHLEM.BRIAN@cusd80.com>
2019-11-20 13:40:19 -0700

    comments and some refactoring

--- guessingGame.js
+++ guessingGame.js
@@ -1,24 +1,55 @@
+/**
+ * The controller for a number guessing game where the user or computer
+ * picks a secret number and the other guesses it.
+ * 
+ * @type GuessingGame
+ */
 export class GuessingGame {
+    /**
+     * Create a new guessing game allowing numbers from 1..maxNumber
+     * @param {type} maxNumber the highest number allowed for guesses
+     */
     constructor (maxNumber) {
         this.newGame(maxNumber);
     }
     
+    /**
+     * Get the maximum possible number for this guessing game
+     * @returns {Number}
+     */
     get max() {
         return this.maxNum;
     }
-            
+    
+    /**
+     * Get the maximum number of guesses that it should take to guess the number
+     * @returns {Number}
+     */
     get guessLimit() {
         return this.maxGuesses;
     };
     
+    /**
+     * Get the history of all guesses the user has made in this game
+     * @returns {Array}
+     */
     get userHistory() {
         return this.userGuesses;
     }
     
+    /**
+     * Get the number the user is guessing
+     * @returns {Number}
+     */
     get secretNumber() {
         return this.secretNum;
     }
     
+    /**
+     * Initialize (restart) a new guessing game
+     * @param {Number} maxNumber the highest number to allow for guessing
+     * @returns {undefined}
+     */
     newGame(maxNumber) {
         this.maxNum = maxNumber;
         this.maxGuesses = Math.ceil(Math.log2(maxNumber));
@@ -31,6 +62,14 @@ export class GuessingGame {
         this.guessMax = maxNumber;
     };
     
+    /**
+     * The user has made a guess, evaluate it.
+     * 
+     * @param {Number} guess the number the player guessed
+     * @returns {undefined|Object} undefined if the number is invalid,
+     *          An object with properties val: the number guessed,
+     *          prompt: the relation of the secret number to that guess
+     */
     madeGuess(guess) {
         if (guess === undefined || guess === "") {
             return;
@@ -67,20 +106,34 @@ export class GuessingGame {
         return rval;
     };
     
+    /**
+     * Have the computer generate a guess for a user's secret number
+     * 
+     * @returns {Number} the number the computer guessed
+     */
     makeGuess() {
         if (this.guessMin > this.guessMax) {
             return -1;
         }
         
-        this.guess = Math.floor((this.guessMax - this.guessMin) / 2) + this.guessMin;
+        this.guess = Math.floor((this.guessMax + this.guessMin) / 2);
         
         return this.guess;
     }
     
+    /**
+     * Get the list of guesses the computer has made and their evaluations
+     * @returns {Array} The history of the computer's guesses
+     */
     get guessHistory() {
         return this.guesses;
     }
     
+    /**
+     * Have the computer guess again, this time a higher number
+     * 
+     * @returns {Number} The computer's guess
+     */
     guessHigher() {
         this.guesses.push({val: this.guess, prompt: "HIGHER"});
         this.guessMin = this.guess + 1;
@@ -88,6 +141,11 @@ export class GuessingGame {
         return this.makeGuess();
     }
     
+    /**
+     * Have the computer guess again, this time a lower number
+     * 
+     * @returns {Number} The computer's guess
+     */
     guessLower() {
         this.guesses.push({val: this.guess, prompt: "LOWER"});
         this.guessMax = this.guess - 1;
@@ -95,16 +153,15 @@ export class GuessingGame {
         return this.makeGuess();
     }
     
+    /**
+     * The computer's guess was correct. Mark it in the history
+     * 
+     * @returns {undefined}
+     */
     guessGood() {
         if (this.guesses.length === 0 || 
                 this.guesses[this.guesses.length - 1].val !== this.guess) {
             this.guesses.push({val: this.guess, prompt: "GOT IT!"});
-        }
-        
-        return;
+        }       
     }
-}
-
-export function updateDisplay(val, disp) {
-    $(disp).text(val); 
 }
